8937b480aed69f699a45fc5901fe0a7b395acd7b
Porfírio Ribeiro <porfirioribeiro@gmail.com>
2016-07-15 22:35:40 +0100

    Optimized production code to be small

--- webpack.config.js
+++ webpack.config.js
@@ -3,16 +3,41 @@ var LiveReloadPlugin = require('webpack-livereload-plugin');
 var webpack=require('webpack');
 var path = require('path');
 
-
+var isProd = (process.env.NODE_ENV === 'production');
 var plugins = [
     new ExtractPlugin('bundle.css'),
     new LiveReloadPlugin({appendScriptTag: true}),
     new webpack.DefinePlugin({
         'process.env': {
-            'NODE_ENV': JSON.stringify('production')
+            'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
         }
-    })
+    }),
+    new webpack.IgnorePlugin(/^jquery$/)
 ];
+
+var devtool="sourcemap";
+
+var babelPresets=['es2015','react'];
+if (isProd){
+    babelPresets.push("react-optimize")
+    devtool="";
+    plugins.push(new webpack.optimize.UglifyJsPlugin({
+        // Don't beautify output (enable for neater output)
+        beautify: false,
+
+        // Eliminate comments
+        comments: false,
+
+        // Compression specific options
+        compress: {
+            warnings: false,
+
+            // Drop `console` statements
+            drop_console: true
+        }
+        }))
+}
+
 
 module.exports = {
     entry: './src',
@@ -20,6 +45,7 @@ module.exports = {
         path: 'builds',
         filename: 'bundle.js',
     },
+    devtool:devtool,
     plugins: plugins,
     resolve: {
         extensions: ['', '.js', '.jsx'],
@@ -30,6 +56,16 @@ module.exports = {
                 test: /\.js/,
                 loader: 'babel',
                 include: __dirname + '/src',
+            },
+            {
+                test: /\.js$/,
+                exclude: /(_node_modules|bower_components)/,
+                loader: 'babel',
+                include: __dirname + '/src',
+                query: {
+                    presets: babelPresets,
+                    plugins: ["transform-object-rest-spread"]
+                }
             },
             {
                 test: /\.scss/,
@@ -45,7 +81,8 @@ module.exports = {
             {
                 test: /\.(png|gif|jpe?g|svg)$/i,
                 loader: 'url?limit=10000',
-            }
+            },
+            { test: /backbone\.js$/, loader: 'imports?define=>false' }
         ]
     }
 };
