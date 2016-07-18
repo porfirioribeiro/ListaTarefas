8fa4d94cec81030f1827d7c5f3921df688e415ed
Porfírio Ribeiro <porfirioribeiro@gmail.com>
2016-07-18 23:15:41 +0100

    Eslint warn about stateless function, made Footer a stateless function

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,42 +6,40 @@ import React from "react";
 
 /**
  * Footer, component to show the footer of the app, contains info and some actions
+ * Todo split this into other components
+ * @param {Number} remaining
+ * @param {Number} done
+ * @param {Number} all
+ * @param {Function} onMarkAllAsDone
+ * @param {Function} onDeleteAllDone
+ * @returns {XML}
+ * @constructor
  */
-export default class Footer extends React.Component {
-    constructor(props) {
-        super(props)
-    }
+export default function Footer({remaining, done, all, onMarkAllAsDone, onDeleteAllDone}) {
+    let markAllAsDone, deleteDone, remainingDiv, linkBar;
 
-    render() {
-        let markAllAsDone, deleteDone, remaining, linkBar;
+    if (remaining > 0) {
+        markAllAsDone = <a href="javascript:;" onClick={onMarkAllAsDone}>Marcar todas ({remaining}) como completas</a>;
+        remainingDiv = <div className="info-line">Ainda falta completar {remaining} tarefa(s)!</div>
+    } else if (all > 0)
+        remainingDiv = <div className="info-line">Todas as tarefas foram completadas!!</div>;
+    else
+        remainingDiv = <div className="info-line">Não tem nenhuma tarefa!</div>;
 
-        if (this.props.remaining > 0) {
-            markAllAsDone =
-                <a href="javascript:;" onClick={this.props.onMarkAllAsDone}>Marcar todas ({this.props.remaining})
-                    como completas</a>;
-            remaining = <div className="info-line">Ainda falta completar {this.props.remaining} tarefa(s)!</div>
-        } else if (this.props.all > 0)
-            remaining = <div className="info-line">Todas as tarefas foram completadas!!</div>;
-        else
-            remaining = <div className="info-line">Não tem nenhuma tarefa!</div>;
+    if (done > 0)
+        deleteDone = <a href="javascript:;" onClick={onDeleteAllDone}>Apagar todas ({done}) as completas</a>;
 
-        if (this.props.done > 0)
-            deleteDone =
-                <a href="javascript:;" onClick={this.props.onDeleteAllDone}>Apagar todas ({this.props.done}) as
-                    completas</a>;
-
-        if (!!markAllAsDone || !!deleteDone)
-            linkBar = (<div className="link-bar">
-                {markAllAsDone}
-                {deleteDone}
-            </div>);
+    if (!!markAllAsDone || !!deleteDone)
+        linkBar = (<div className="link-bar">
+            {markAllAsDone}
+            {deleteDone}
+        </div>);
 
 
-        return (<footer>
-            {linkBar}
-            {remaining}
-        </footer>)
-    }
+    return (<footer>
+        {linkBar}
+        {remainingDiv}
+    </footer>)
 }
 
 const {number, func}=React.PropTypes;
